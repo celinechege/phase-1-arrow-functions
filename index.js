aa806c5d4a88c14ae6b4e49108fb3c43b26488ae
celinechege <chegeceline18@gmail.com>
2023-03-19 00:37:42 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// Define divide function expression
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Define square arrow function
+const square = (num) => {
+  return num * num;
+};
+
+// Define add arrow function
+const add = (num1, num2) => {
+  return num1 + num2;
+};
